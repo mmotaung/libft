a21dbf62045d1d444f9d2a3bf702bbc8b93612ff
Modikadika MOTAUNG <mmotaung@c4r10s1.wethinkcode.co.za>
2019-06-20 12:55:02 +0200

    working ft_strsub

---
+++ ft_strsub.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmotaung <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/03 14:06:54 by mmotaung          #+#    #+#             */
+/*   Updated: 2019/06/14 07:57:30 by mmotaung         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	unsigned int	i;
+	char			*str;
+
+	i = 0;
+	if (!s || !(str = (char *)malloc(sizeof(char) * len + 1)))
+		return (NULL);
+	while (i < len)
+	{
+		str[i] = s[i + start];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
