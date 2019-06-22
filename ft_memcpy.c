40c58f9faf6762aa1db2dfc68cbc6a6c356c1dc8
Modikadika MOTAUNG <mmotaung@c4r10s1.wethinkcode.co.za>
2019-06-22 13:24:47 +0200

    fixed ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,24 +6,24 @@
 /*   By: mmotaung <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 12:37:20 by mmotaung          #+#    #+#             */
-/*   Updated: 2019/06/12 07:49:19 by mmotaung         ###   ########.fr       */
+/*   Updated: 2019/06/22 12:33:07 by mmotaung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *s1, const void *s2, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	unsigned char	*i;
 	unsigned char	*j;
 
-	if (s1 == s2)
-		return ((char *)s2);
-	if (n == 0 && s1 == s2)
-		return (s1);
-	i = (unsigned char *)s1;
-	j = (unsigned char *)s2;
+	if (dst == src)
+		return ((char *)src);
+	if (n == 0 && dst == src)
+		return (dst);
+	i = (unsigned char *)dst;
+	j = (unsigned char *)src;
 	while (n--)
 		*i++ = *j++;
-	return (s1);
+	return (dst);
 }
