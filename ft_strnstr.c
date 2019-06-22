cb63c6b712d254fd38e678e084827287f4d77ff1
Modikadika MOTAUNG <mmotaung@c4r10s1.wethinkcode.co.za>
2019-06-22 13:27:17 +0200

    fixed ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,24 +6,24 @@
 /*   By: mmotaung <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 14:37:48 by mmotaung          #+#    #+#             */
-/*   Updated: 2019/06/12 10:37:51 by mmotaung         ###   ########.fr       */
+/*   Updated: 2019/06/22 12:48:50 by mmotaung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *s1, const char *s2, size_t n)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 
-	if (*s2 == '\0')
-		return ((char *)s1);
-	i = ft_strlen(s2);
-	while (*s1 != '\0' && n-- >= i)
+	if (*needle == '\0')
+		return ((char *)haystack);
+	i = ft_strlen(needle);
+	while (*haystack != '\0' && len-- >= i)
 	{
-		if (*s1 == *s2 && ft_memcmp(s1, s2, i) == 0)
-			return ((char *)s1);
-		s1++;
+		if (*haystack == *needle && ft_memcmp(haystack, needle, i) == 0)
+			return ((char *)haystack);
+		haystack++;
 	}
 	return (NULL);
 }
