702078f07a49f6fbf43edbacd06b268dcc042562
Modikadika MOTAUNG <mmotaung@c4r10s1.wethinkcode.co.za>
2019-06-22 13:26:36 +0200

    fixed ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,28 +6,28 @@
 /*   By: mmotaung <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 12:35:18 by mmotaung          #+#    #+#             */
-/*   Updated: 2019/06/13 14:42:09 by mmotaung         ###   ########.fr       */
+/*   Updated: 2019/06/22 12:42:00 by mmotaung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t		ft_strlcat(char *dst, const char *src, size_t size)
+size_t		ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	j;
 
 	i = 0;
 	j = 0;
-	while (dst[i] && i < size)
+	while (dst[i] && i < dstsize)
 		i++;
 	j = i;
-	while (src[i - j] && i + 1 < size)
+	while (src[i - j] && i + 1 < dstsize)
 	{
 		dst[i] = src[i - j];
 		i++;
 	}
-	if (j < size)
+	if (j < dstsize)
 		dst[i] = '\0';
 	return (j + ft_strlen(src));
 }
