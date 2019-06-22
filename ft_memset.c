c8166e3896c563b8929e0f0b65ec76d2e2d2ebce
Modikadika MOTAUNG <mmotaung@c4r10s1.wethinkcode.co.za>
2019-06-22 13:25:29 +0200

    fixed ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,23 +6,23 @@
 /*   By: mmotaung <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 06:59:08 by mmotaung          #+#    #+#             */
-/*   Updated: 2019/06/12 07:21:22 by mmotaung         ###   ########.fr       */
+/*   Updated: 2019/06/22 12:27:47 by mmotaung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *s, char c, size_t n)
+void	*ft_memset(void *b, int c, size_t len)
 {
 	unsigned int	i;
 	char			*j;
 
 	i = 0;
-	j = (char*)s;
-	while (i < n)
+	j = (char*)b;
+	while (i < len)
 	{
 		j[i] = (unsigned char)c;
 		i++;
 	}
-	return (s);
+	return (b);
 }
