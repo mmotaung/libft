587041ecbdb6e3d4b848cb0c0c8be52155f680ff
Modikadika MOTAUNG <mmotaung@c4r10s1.wethinkcode.co.za>
2019-06-22 13:26:14 +0200

    fixed ft_strncpy

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,23 +6,23 @@
 /*   By: mmotaung <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 09:04:51 by mmotaung          #+#    #+#             */
-/*   Updated: 2019/05/28 09:31:06 by mmotaung         ###   ########.fr       */
+/*   Updated: 2019/06/22 12:39:17 by mmotaung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strncpy(char *dst, const char *src, size_t n)
+char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
 	unsigned int	i;
 
 	i = 0;
-	while (src[i] && i < n)
+	while (src[i] && i < len)
 	{
 		dst[i] = src[i];
 		i++;
 	}
-	while (i < n)
+	while (i < len)
 	{
 		dst[i] = '\0';
 		i++;
