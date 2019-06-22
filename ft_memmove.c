b8808088df763ef79fa3ac2b38edf31879a82712
Modikadika MOTAUNG <mmotaung@c4r10s1.wethinkcode.co.za>
2019-06-22 13:25:52 +0200

    fixed ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,13 +6,13 @@
 /*   By: mmotaung <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 09:00:30 by mmotaung          #+#    #+#             */
-/*   Updated: 2019/06/10 08:59:08 by mmotaung         ###   ########.fr       */
+/*   Updated: 2019/06/22 12:35:09 by mmotaung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t n)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	unsigned int	i;
 
@@ -20,10 +20,10 @@ void	*ft_memmove(void *dst, const void *src, size_t n)
 		return ((char *)src);
 	i = 0;
 	if (((unsigned char *)src) < ((unsigned char *)dst))
-		while (n-- > 0)
-			((unsigned char *)dst)[n] = ((unsigned char *)src)[n];
+		while (len-- > 0)
+			((unsigned char *)dst)[len] = ((unsigned char *)src)[len];
 	else
-		while (i < n)
+		while (i < len)
 		{
 			((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
 			i++;
